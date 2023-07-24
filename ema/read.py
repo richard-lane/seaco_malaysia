5fdf5f8c228ddafa35cff095f7674605056ee62e
Richard Lane <richard.lane6798@gmail.com>
2023-07-24 14:21:32 +0100

    accel info fcn

--- ema/read.py
+++ ema/read.py
@@ -3,6 +3,8 @@ Helpers for reading data
 
 """
 import pandas as pd
+from openmovement.load import CwaData
+
 
 def meal_info(participant_id: str) -> pd.DataFrame:
     """
@@ -19,3 +21,20 @@ def meal_info(participant_id: str) -> pd.DataFrame:
 
     df = pd.read_csv(path)
     return df[df["p_id"] == p_id]
+
+
+def accel_info(filepath: str) -> pd.DataFrame:
+    """
+    Get accelerometer data from a CWA file
+
+    This file should be an AX6 database that includes accelerometry and
+    gyroscopic data.
+    These files are Big (and accessed over the network if using RDSF drive),
+    which means this function might be slow
+
+    :param filepath: path to the CWA file
+    :returns: the accelerometer, gyroscope and time data
+
+    """
+    with CwaData(filepath, include_accel=True, include_gyro=True) as cwa_data:
+        return cwa_data.get_samples()
