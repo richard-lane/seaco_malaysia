e9c1b4ca25a429df2d0f02707964fd41dff7c06a
Richard Lane <richard.lane@bristol.ac.uk>
2024-01-03 13:22:32 +0000

    broken env.yml

---
+++ mlm_pipeline/r/day_model.R
@@ -0,0 +1,14 @@
+install.packages("lme4", repos="https://www.stats.bris.ac.uk/R/")
+library(lme4)
+library(tidyverse)
+
+# Read the data
+model_df <- read_csv("mlm_pipeline/data/model_df.csv")
+
+# Define some models
+
+# Extract random effects
+
+# Plot histograms
+
+# Anova
