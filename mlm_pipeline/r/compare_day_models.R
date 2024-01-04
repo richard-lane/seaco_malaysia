293344646cc510e10aa13162bef30b46d0b3f7f8
Richard Lane <richard.lane@bristol.ac.uk>
2024-01-04 16:33:35 +0000

    fcn for plotting

--- mlm_pipeline/r/compare_day_models.R
+++ mlm_pipeline/r/compare_day_models.R
@@ -4,6 +4,8 @@
 library(lme4)
 library(tidyverse)
 library(sjPlot)
+library(dplyr)
+library(scales)
 
 # Read the data
 model_df <- read_csv("mlm_pipeline/data/model_df.csv")
@@ -13,17 +15,16 @@ fixed_only <- glm(entry ~ day, data = model_df, family = binomial(link = "logit"
 random_intercept <- glmer(entry ~ day + (1 | p_id), data = model_df, family = binomial(link = "logit"))
 random_both <- glmer(entry ~ day + (1 + day | p_id), data = model_df, family = binomial(link = "logit"))
 
-fixed_plot <- plot_model(fixed_only, type = "eff", terms = "day", show.rug = TRUE)
-fixed_plot + scale_y_continuous(limits = c(0.0, 1.0), label = scales::percent_format(accuracy = 10))
-ggsave("fixed_only_fit.png")
+plot_and_save <- function(model, filename) {
+    plot <- plot_model(model, type = "eff", terms = "day", show.rug = TRUE)
+    plot <- plot + scale_y_continuous(limits = c(0.0, 1.0), label = percent_format(accuracy = 10))
+    ggsave(filename, plot)
+}
 
-intercept_plot <- plot_model(random_intercept, type = "eff", terms = "day", show.rug = TRUE)
-intercept_plot + scale_y_continuous(limits = c(0.0, 1.0), label = scales::percent_format(accuracy = 10))
-ggsave("random_intercepts.png")
+plot_and_save(fixed_only, "fixed_only_fit.png")
+plot_and_save(random_intercept, "random_intercepts.png")
+plot_and_save(random_both, "random_pids_fit.png")
 
-full_plot <- plot_model(random_both, type = "eff", terms = "day", show.rug = TRUE)
-full_plot + scale_y_continuous(limits = c(0.0, 1.0), label = scales::percent_format(accuracy = 10))
-ggsave("random_pids_fit.png")
 
 # Plot histograms
 random_effects <- ranef(random_both)[1]$p_id
