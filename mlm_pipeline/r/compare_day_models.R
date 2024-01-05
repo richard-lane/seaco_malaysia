bff0d4557fd1db076a8ecdbfc0a8cd5694e93d33
Richard Lane <richard.lane@bristol.ac.uk>
2024-01-05 10:35:08 +0000

    plot models with all the fits in

--- mlm_pipeline/r/compare_day_models.R
+++ mlm_pipeline/r/compare_day_models.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(sjPlot)
 library(dplyr)
 library(scales)
+library(ggplot2)
 
 # Read the data
 model_df <- read_csv("mlm_pipeline/data/model_df.csv")
@@ -22,14 +23,41 @@ percentage_yes <- model_df %>%
 
 plot_and_save <- function(model, filename) {
     plot <- plot_model(model, type = "pred", terms = "day", show.rug = TRUE)
-    plot <- plot + geom_point(data = percentage_yes, aes(x = day, y = percentage_yes / 100), color = "black")
+
+    plot <- plot + geom_point(data = percentage_yes, aes(x = day, y = percentage_yes / 100), color = "#653D9BC4")
     plot <- plot + scale_y_continuous(limits = c(0.0, 1.0), label = percent_format(accuracy = 10))
+
     ggsave(filename, plot)
 }
 
 plot_and_save(fixed_only, "fixed_only_fit.png")
 plot_and_save(random_intercept, "random_intercepts.png")
 plot_and_save(random_both, "random_pids_fit.png")
+
+plot_individual_pids <- function(model, filename) {
+    # Create a new data frame with the unique days and p_ids
+    newdata <- expand.grid(day = unique(model_df$day), p_id = unique(model_df$p_id))
+
+    # Add the predicted values to the new data frame
+    newdata$pred <- predict(model, newdata = newdata, type = "response")
+
+    # Convert p_id to a factor
+    newdata$p_id <- as.factor(newdata$p_id)
+
+    # Create the overall plot
+    plot <- plot_model(model, type = "pred", terms = "day", show.rug = TRUE)
+
+    # Add the predicted values for each p_id to the plot
+    plot <- plot + geom_line(data = newdata, aes(x = day, y = pred, color = p_id), alpha = 0.5) +
+        scale_color_manual(values = rep("skyblue", 83)) +
+        geom_point(data = percentage_yes, aes(x = day, y = percentage_yes / 100), color = "black") +
+        scale_y_continuous(limits = c(0.0, 1.0), label = scales::percent_format(accuracy = 10)) +
+        theme(legend.position = "none")
+
+    ggsave(filename, plot)
+}
+plot_individual_pids(random_intercept, "random_intercepts_all.png")
+plot_individual_pids(random_both, "random_pids_fit_all.png")
 
 
 # Plot histograms
