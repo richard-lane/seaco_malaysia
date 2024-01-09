f29ef6017274cf58cd41e1470f0737f6b42b2451
Richard Lane <richard.lane@bristol.ac.uk>
2024-01-09 10:59:23 +0000

    add number of days in school + age over 12 flags. Some NaNs in the school responses

--- mlm_pipeline/python/create_csv.py
+++ mlm_pipeline/python/create_csv.py
@@ -5,6 +5,7 @@ ready to run a multi-level model
 """
 import sys
 import pathlib
+import numpy as np
 import pandas as pd
 
 sys.path.append(str(pathlib.Path(__file__).parent.parents[1].absolute()))
@@ -29,7 +30,13 @@ def main():
     demographic_df = read._qnaire_df()
     demographic_df = demographic_df[demographic_df["respondent_status"] == 1]
     demographic_df = demographic_df[
-        ["residents_id", "respondent_sex", "respondent_ethnicity", "age_dob"]
+        [
+            "residents_id",
+            "respondent_sex",
+            "respondent_ethnicity",
+            "age_dob",
+            "phyactq1",
+        ]
     ]
     model_df = (
         model_df.reset_index()
@@ -40,22 +47,32 @@ def main():
     # Add a column indicating whether the participants are over the age of 12
     model_df["age_group"] = (model_df["age_dob"] > 12).astype(int)
 
-    model_df.rename(
-        columns={
-            "respondent_sex": "sex",
-            "respondent_ethnicity": "ethnicity",
-        },
-        inplace=True,
-    )
-
     # Whether each day was a weekend or weekday
     model_df["weekend"] = meal_info.index.dayofweek.isin({5, 6}).astype(int)
+
+    # How many days each participant spent in school
+    model_df["over_2_days_in_school"] = (model_df["phyactq1"] > 2).astype(int)
+
+    # For now, if the participant didn't answer the days in school question just set their answer to NaN
+    # TODO a better solution
+    model_df.loc[
+        (model_df["phyactq1"] == -99) | (model_df["phyactq1"].isnull()),
+        "over_2_days_in_school",
+    ] = np.nan
 
     # Whether each entry was a reponse or not
     model_df["entry"] = (
         meal_info["meal_type"]
         .isin({"Meal", "Drink", "Snack", "No food/drink"})
         .astype(int)
+    )
+
+    model_df.rename(
+        columns={
+            "respondent_sex": "sex",
+            "respondent_ethnicity": "ethnicity",
+        },
+        inplace=True,
     )
 
     model_df.to_csv(
