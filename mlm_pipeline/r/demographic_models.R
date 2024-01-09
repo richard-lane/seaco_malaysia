4b9d08066fb31fa7b991d0eb1efbcfb7838ff0a5
Richard Lane <richard.lane@bristol.ac.uk>
2024-01-09 11:21:20 +0000

    school model, change optimiser + add more iterations

--- mlm_pipeline/r/demographic_models.R
+++ mlm_pipeline/r/demographic_models.R
@@ -9,9 +9,11 @@ library(ggplot2)
 model_df <- read_csv("mlm_pipeline/data/model_df.csv")
 
 # Define some models
-sex_model <- glmer(entry ~ day * sex + (1 + day | p_id), data = model_df, family = binomial(link = "logit"))
-ethnicity_model <- glmer(entry ~ day * ethnicity + (1 + day | p_id), data = model_df, family = binomial(link = "logit"))
-age_model <- glmer(entry ~ day * age_group + (1 + day | p_id), data = model_df, family = binomial(link = "logit"))
+control <- glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 2e5))
+sex_model <- glmer(entry ~ day * sex + (1 + day | p_id), data = model_df, family = binomial(link = "logit"), control = control)
+ethnicity_model <- glmer(entry ~ day * ethnicity + (1 + day | p_id), data = model_df, family = binomial(link = "logit"), control = control)
+age_model <- glmer(entry ~ day * age_group + (1 + day | p_id), data = model_df, family = binomial(link = "logit"), control = control)
+school_model <- glmer(entry ~ day * over_2_days_in_school + (1 + day | p_id), data = model_df, family = binomial(link = "logit"), control = control)
 
 # Find what percentage of positive entries there were on each day
 percentage_yes <- model_df %>%
@@ -30,7 +32,9 @@ plot_and_save <- function(model, filename) {
 plot_and_save(sex_model, "mlm_pipeline/outputs/sex_fit.png")
 plot_and_save(ethnicity_model, "mlm_pipeline/outputs/ethnicity_fit.png")
 plot_and_save(age_model, "mlm_pipeline/outputs/age_fit.png")
+plot_and_save(school_model, "mlm_pipeline/outputs/school_fit.png")
 
 capture.output(summary(sex_model), file = "sex_model.txt")
 capture.output(summary(ethnicity_model), file = "ethnicity_model.txt")
 capture.output(summary(age_model), file = "age_model.txt")
+capture.output(summary(school_model), file = "school_model.txt")
