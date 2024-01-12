3d44f69fc292ca58b2625e057d34e818f28a8b66
Richard Lane <richard.lane@bristol.ac.uk>
2024-01-12 10:33:46 +0000

    demographic pltos

--- mlm_pipeline/python/demographic_plots.py
+++ mlm_pipeline/python/demographic_plots.py
@@ -149,8 +149,64 @@ def school_plot(demographic_df):
     school_stacked_bar(demographic_df)
 
 
-def weekdays_plot():
-    """ """
+def weekday_hist(demographic_df, day_lookup):
+    fig, ax = plt.subplots()
+
+    bins = np.arange(1, 8)
+    bins = np.concatenate([bins - 0.2, bins + 0.2])
+    bins = np.sort(bins)
+
+    ax.hist(demographic_df["day"], bins=bins)
+    ax.set_ylabel("Number of Entries")
+    ax.set_xlabel("Day")
+
+    ax.set_xticks(range(1, 8), [day_lookup[i] for i in range(1, 8)])
+
+    fig.tight_layout()
+    fig.savefig("mlm_pipeline/outputs/demographics/weekday_hist.png")
+
+
+def weekday_stacked_bar(demographic_df, day_lookup):
+    # Stacked bar plot
+    fig, ax = plt.subplots()
+
+    bottom = 0
+    for day in range(1, 6):
+        count = (demographic_df["day"] == day).sum()
+        ax.bar(0, count, bottom=bottom)
+        if count > 2:
+            ax.text(0, bottom + count / 2, day_lookup[day], ha="center", va="center")
+        bottom += count
+
+    bottom = 0
+    for day in range(6, 8):
+        count = (demographic_df["day"] == day).sum()
+        ax.bar(1, count, bottom=bottom)
+        if count > 2:
+            ax.text(1, bottom + count / 2, day_lookup[day], ha="center", va="center")
+        bottom += count
+
+    ax.set_xticks((0, 1), ("Weekday", "Weekend"))
+
+    ax.set_ylabel("Number of Entries")
+    ax.set_xlabel("Day")
+
+    fig.tight_layout()
+    fig.savefig("mlm_pipeline/outputs/demographics/weekday_stacked.png")
+
+
+def weekdays_plot(demographic_df):
+    day_lookup = {
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+        7: "Sunday",
+    }
+    weekday_hist(demographic_df, day_lookup)
+    weekday_stacked_bar(demographic_df, day_lookup)
 
 
 def main():
@@ -159,15 +215,16 @@ def main():
 
     entries_df = pd.read_csv("mlm_pipeline/data/model_df.csv")
 
-    # Only keep the first row for each participant
+    # These plots are for all entries
+    weekdays_plot(entries_df)
+
+    # Throw away all but one entry per participant
+    # This means that subsequent plots are for participants, not entries
     entries_df = entries_df.drop_duplicates(subset="p_id")
 
     age_hists(entries_df)
-
     ethnicity_plot(entries_df)
-
     sex_plot(entries_df)
-
     school_plot(entries_df)
 
 
